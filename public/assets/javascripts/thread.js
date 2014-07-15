7b3be75a3e9c898bbcbbacf920d3e26b8779a3e0
Thomas Lekanger <datagutt@lekanger.no>
2014-07-15 23:55:45 +0200

    Client-side part of quoting

--- public/assets/javascripts/thread.js
+++ public/assets/javascripts/thread.js
@@ -3,7 +3,7 @@
 	Site.Thread = {};
 	Site.Thread.init = function(){
 		var self = this;
-		if(ID.areFeatures('query', 'attachListener')){
+		if(ID.areFeatures('query', 'queryOne', 'attachListener', 'getElementData')){
 			var postNos = ID.query('.ref-link.no');
 			ID.forEach(postNos, function(postNo){
 				ID.attachListener(postNo, 'click', function(e){
@@ -13,16 +13,34 @@
 			});
 
 			var postVals = ID.query('.ref-link.val');
+			// Different IDs are used (thread-form, reply-form), so i do this hacky selector instead
+			var textarea = ID.queryOne('.pure-form textarea[name="comment"]');
 			ID.forEach(postVals, function(postVal){
 				ID.attachListener(postVal, 'click', function(e){
 					var num = ID.getElementData(postVal, 'num');
-					// TODO: Add quoting (>> num in textarea)
+					ID.setInputValue(textarea, '>>' + num + '\n');
+					textarea.focus();
 				});
 			});
+
+			var matches = window.location.hash.match(/^#(i)?(\d+)$/);
+			if(!matches){
+				return;
+			}
+
+			var doInsert = typeof matches[1] != 'undefined';
+			var num = matches[2];
+
+			if(doInsert && (textarea && !textarea.value)){
+				ID.setInputValue(textarea, '>>' + num + '\n');
+				textarea.focus();
+				return;
+			}
+
+			self.highlightPost(num);
 		}
 	};
 	Site.Thread.highlightPost = function(num){
-		// Empty
 		var highlighted = ID.queryOne('.highlighted');
 		var post = ID.queryOne('#p' + num);
 		if(highlighted){
